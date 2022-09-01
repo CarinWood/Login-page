7142fab865003ac7ce62c43606a4d94fe933b7ec
Carin Wood <carin.wood.85@gmail.com>
2022-09-01 14:17:23 +0200

    design and responsive design update

--- components/form/Form.js
+++ components/form/Form.js
@@ -4,14 +4,22 @@ const Form = () => {
   return (
     <form className={styles.wrapper}>
         <div className={styles.left}>
-            <input className={styles.input} placeholder="First Name"/>
-            <input className={styles.input} placeholder="Last Name"/>
-            <input className={styles.input} placeholder="Email"/>
+            <input type="text" className={styles.input} placeholder="First Name*"/>
+            <input type="text" className={styles.input} placeholder="Last Name*"/>
+            <input type="text" className={styles.input} placeholder="Email*"/>
         </div>
         <div className={styles.right}>
-            <input className={styles.input} placeholder="Password" />
-            <input className={styles.input} placeholder="Confirm Password"/>
+            <input type="password" className={styles.input} placeholder="Password*" />
+            <input type="password" className={styles.input} placeholder="Confirm Password*"/>
+            
+            <div className={styles.checkboxDiv}>
+              <input type="checkbox" id="terms" name="terms" value="terms"/>
+              <label className={styles.label} for="terms"><i>I agree to terms and conditions</i></label>
+            </div>
+          
+            <button className={styles.button} type="button">Create Account</button>
         </div>
+       
     </form>
   )
 }
